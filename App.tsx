d0f9f83316c0043eaa813dae6d3237b24f24a251
Adeilton Meneses <adeiltonmeneses@MMB027534CPS.local>
2025-09-02 19:26:30 -0300

    configuracao inicial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+import { EditScreenInfo } from 'components/EditScreenInfo';
 
 import './global.css';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <ScreenContent
+        title="Home"
+        path="App.tsx"
+        // children={<Text className="text-red-500">Teste children</Text>}
+        children={<EditScreenInfo path="App.tsx" />}
+      ></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
